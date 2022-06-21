17a058cca572ccc27ea06acb13fbb3b62f4a35c4
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2022-06-21 08:44:17 +0300

    #2 Register: layout

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  public form!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.valid);
+  }
+}
